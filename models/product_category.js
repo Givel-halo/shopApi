725897c985b54381581290d1fd8ec1b9cc6611f5
givel <peizeng.guo@elitesland.com>
2021-10-12 17:51:05 +0800

    nodejs 小米商城爬虫接口

---
+++ models/product_category.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const productCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    descriptions: {
+      type: String
+    },
+    coverImg: {
+      type: String
+    },
+    tag: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("ProductCategory", productCategorySchema);
